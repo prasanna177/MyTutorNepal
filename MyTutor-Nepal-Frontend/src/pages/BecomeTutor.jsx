ba633fef04a68725b16a40c13613c0f6d80891d5
Prasanna Chand <prasannachand77@gmail.com>
2024-02-03 00:44:40 +0545

    last try for frontend validation for unique subjects

--- MyTutor-Nepal-Frontend/src/pages/BecomeTutor.jsx
+++ MyTutor-Nepal-Frontend/src/pages/BecomeTutor.jsx
@@ -3,15 +3,30 @@ import {
   Button,
   FormControl,
   FormErrorMessage,
+  HStack,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Select,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
 import Layout from "../components/Layout/Layout";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { useFieldArray, useForm } from "react-hook-form";
 import TextField from "../components/common/TextField";
+import { DevTool } from "@hookform/devtools";
+import toast from "react-hot-toast";
+import { useState } from "react";
+import { CloseIcon } from "@chakra-ui/icons";
 
 const BecomeTutor = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const schema = yup.object({
     fullName: yup.string().required("Full Name is required"),
     email: yup.string().required("Email address is required"),
@@ -28,6 +43,7 @@ const BecomeTutor = () => {
 
   const onSubmit = (data) => {
     console.log(data);
+    console.log(errors);
   };
 
   const subjectsOptions = [
@@ -44,9 +60,9 @@ const BecomeTutor = () => {
     control,
     formState: { errors },
   } = useForm({
-    defaultValues: {
-      subjects: [""],
-    },
+    // defaultValues: {
+    //   subjects: [""],
+    // },
     resolver: yupResolver(schema),
   });
 
@@ -54,6 +70,10 @@ const BecomeTutor = () => {
     name: "subjects",
     control,
   });
+
+  if (fields.length === 0) {
+    append("");
+  }
 
   return (
     <Layout>
@@ -79,34 +99,55 @@ const BecomeTutor = () => {
         {fields.map((field, index) => (
           <Box key={field.id}>
             <FormControl isInvalid={Boolean(errors?.subjects?.[index])}>
-              <Select
-                name={`subjects[${index}]`}
-                placeholder="Select a subject"
-                {...register(`subjects[${index}]`)}
-              >
-                {subjectsOptions.map((subject) => (
-                  <option key={subject} value={subject}>
-                    {subject}
-                  </option>
-                ))}
-              </Select>
-              <FormErrorMessage>
-                {errors.subjects?.[index]?.message}
-              </FormErrorMessage>
+              <HStack>
+                <Box>
+                  <Select
+                    name={`subjects[${index}]`}
+                    placeholder="Select a subject"
+                    {...register(`subjects[${index}]`)}
+                  >
+                    {subjectsOptions.map((subject) => (
+                      <option key={subject} value={subject}>
+                        {subject}
+                      </option>
+                    ))}
+                  </Select>
+                  <FormErrorMessage>
+                    {errors.subjects?.[index]?.message}
+                  </FormErrorMessage>
+                </Box>
 
-              {index > 0 && (
-                <Button type="button" onClick={() => remove(index)}>
-                  Remove
-                </Button>
-              )}
+                {index > 0 && (
+                  <CloseIcon
+                    boxSize={3}
+                    _hover={{cursor: "pointer"}}
+                    type="button"
+                    onClick={() => remove(index)}
+                  >
+                    Remove
+                  </CloseIcon>
+                )}
+              </HStack>
             </FormControl>
           </Box>
         ))}
-        <Button type="button" onClick={() => append({})}>
+        <Button type="button" onClick={() => append("")}>
           Add Subject
         </Button>
         <Button type="submit">Submit</Button>
       </Box>
+
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Error</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>{errors.subjects?.message}</Text>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <DevTool control={control} />
     </Layout>
   );
 };
