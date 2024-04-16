891357e1a0546d0684814ac3277daf97c92df3e1
Prasanna Chand <prasannachand77@gmail.com>
2024-04-16 14:16:33 +0545

    asd

--- MyTutor-Nepal-Backend/app.js
+++ MyTutor-Nepal-Backend/app.js
@@ -22,10 +22,10 @@ app.use(cors({
   credentials: true
 }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); 
 app.use("/uploads", express.static("uploads"));
 app.use(express.json()); //this is used to destructure username and password sent by the client in json format
-const PORT = `${process.env.SERVER_PORT}` || 6000;
+const PORT = 4000 || 6000;
 
 //routes
 app.use("/api/user", userRoute);
